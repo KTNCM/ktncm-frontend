0d015dcf9adc3c76cb21c4aef50320e259c5d33a
Gerixmus <gerixmuss@gmail.com>
2025-01-26 02:36:15 +0100

    docs: added tsdocs for storage service

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -9,6 +9,11 @@ export class StorageService {
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  /**
+   * Gets an item from local storage.
+   * @param key - Key of the item.
+   * @returns - Item if found or null.
+   */
   getItem(key: string): string | null {
     if (isPlatformBrowser(this.platformId)) {
       return localStorage.getItem(key);
@@ -16,12 +21,21 @@ export class StorageService {
     return null;
   }
 
+  /**
+   * Saves an item under the provided key in local storage.
+   * @param key - Key of the item.
+   * @param value - Value of the item.
+   */
   setItem(key: string, value: string): void {
     if (isPlatformBrowser(this.platformId)) {
       localStorage.setItem(key, value);
     }
   }
 
+  /**
+   * Removes an item for provided key from local storage.
+   * @param key Key of the item.
+   */
   removeItem(key: string): void {
     if (isPlatformBrowser(this.platformId)) {
       localStorage.removeItem(key);
