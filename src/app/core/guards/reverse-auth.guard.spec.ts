330f854b896d4d05580bd853e4e2a591cf222525
Gerixmus <gerixmuss@gmail.com>
2025-01-28 19:21:33 +0100

    test: added unit tests for reverse auth guard

--- src/app/core/guards/reverse-auth.guard.spec.ts
+++ src/app/core/guards/reverse-auth.guard.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { reverseAuthGuard } from './reverse-auth.guard';
+import { AuthService } from '../services/auth.service';
 
 describe('reverseAuthGuard', () => {
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: Router;
+
   const executeGuard: CanActivateFn = (...guardParameters) =>
       TestBed.runInInjectionContext(() => reverseAuthGuard(...guardParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authService = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: authService }
+      ]
+    });
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow navigation if the user is not logged in', () => {
+      authService.isLoggedIn.and.returnValue(true);
+
+      const mockRoute = {} as ActivatedRouteSnapshot;
+      const mockState = {} as RouterStateSnapshot;
+      const result = executeGuard(mockRoute, mockState);
+
+      expect(result).toBeFalse();
+      expect(router.navigate).toHaveBeenCalledWith(['/destinations']);
+  });
+
+  it('should block navigation and redirect to login if the user is logged in', () => {
+    authService.isLoggedIn.and.returnValue(false);
+
+    const mockRoute = {} as ActivatedRouteSnapshot;
+    const mockState = {} as RouterStateSnapshot;
+    const result = executeGuard(mockRoute, mockState);
+
+    expect(result).toBeTrue();
+    expect(router.navigate).not.toHaveBeenCalled();
   });
 });
